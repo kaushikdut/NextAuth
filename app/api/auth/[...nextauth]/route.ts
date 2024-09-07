17bb2086a96bb2dcaee19930f13c6cd4dd43f203
Vicky Dutta <duttakoushik779@gmail.com>
2024-09-07 22:42:05 +0530

    feat: db

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import { db } from "@/libs/db";
+import { db } from "@/lib/db";
 import NextAuth from "next-auth/next";
 import CredentialsProvider from "next-auth/providers/credentials";
 
@@ -25,26 +25,25 @@ const handler = NextAuth({
       },
 
       async authorize(credentials, req) {
-        console.log("trigger");
-        // if (credentials) {
-        //   const { email, password } = credentials;
+        if (credentials) {
+          const { email, password } = credentials;
 
-        //   if (!email || !password) {
-        //     return null;
-        //   }
-        //   console.log(email, password);
+          if (!email || !password) {
+            return null;
+          }
 
-        //   const user = await db.user.findUnique({
-        //     where: {
-        //       email,
-        //     },
-        //   });
+          const user = await db.user.findUnique({
+            where: {
+              email,
+            },
+          });
 
-        //   if (user?.password !== password) {
-        //     return null;
-        //   }
-        //   return user || null;
-        // }
+          if (user) {
+            return { ...user, id: user.id.toString() };
+          }
+
+          return null;
+        }
         return null;
       },
     }),
--- app/login/form.tsx
+++ app/login/form.tsx
@@ -10,11 +10,9 @@ import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
-  FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { toast } from "@/hooks/use-toast";
